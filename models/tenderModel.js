26d99cf97a7c09d1c84ebff2d32083171d927800
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-10-26 20:03:19 +0530

    tender management backend impl

---
+++ models/tenderModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const tender = new mongoose.Schema({
+    siteManagerID: { type: String, required: true },
+    siteManagerName: { type: String, required: true },
+    items: [{type: Object, required: true}],
+    status: { type: String, required: true },
+    expectedBudget: { type: Number, required: true },
+    acceptedSupplier: { type: String, required: false },
+    actualAmount: { type: Number, required: false },
+    createdDate: { type: Date, required: true }
+});
+
+module.exports = mongoose.model("Tender", tender);
