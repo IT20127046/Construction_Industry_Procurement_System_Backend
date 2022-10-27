1c778468e27d73efac6dd95aff77ffceb2e3ad2a
IT20131456 <it20131456@my.sliit.lk>
2022-10-27 21:41:37 +0530

    Update files

--- models/supplierModel.js
+++ models/supplierModel.js
@@ -1,24 +1,46 @@
 const mongoose = require("mongoose");
 
 const supplierSchema = new mongoose.Schema({
-    uid: {
-        type: String,
-        required: true,
-      },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    mobile: { type: String, required: true },
-    address: { type: String, required: true },
-    type: { type: String, required: true },
-    password: {
-        type: String,
-        required: true,
-      },
-    dateRegistered: {
-        type: Date,
-        default: Date.now,
-      }
-
+  uid: {
+    type: String,
+    required: true,
+  },
+  name: { 
+    type: String,
+    required: true
+   },
+  email: { 
+    type: String, 
+    required: true 
+  },
+  mobile: { 
+    type: String, 
+    required: true 
+  },
+  address: { 
+    type: String, 
+    required: true 
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  supplierItems: {
+    type: String,
+    required: false,
+  },
+  type: { 
+    type: String, 
+    required: true 
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateRegistered: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("suppliers", supplierSchema);
--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use(supplierRoutes);
 const tenderRoutes = require('./routes/tenderRoutes');
 app.use(tenderRoutes);
 
-const supplierDetailsRouter = require('./routes/supplierDetailsRouter');
-app.use(supplierDetailsRouter);
-
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
 
